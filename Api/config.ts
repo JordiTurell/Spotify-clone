a295e05156756dda404df0b7c36ef67bce0593f3
jordi.turell <jordi.turell@extremeit.es>
2025-05-16 10:49:53 +0200

    config api

---
+++ Api/config.ts
@@ -0,0 +1,5 @@
+export const configspoti = {
+  clientId: "6fd5523969914d239115e6aca814cd70",
+  clientSecret: "f4f2a1d6f78346ed926b6e138b52efad",
+  redirect_uri: "https://80d1-185-77-185-26.ngrok-free.app/callback",
+};
